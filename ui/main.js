1fd403030b94028c34713ae38efe1c907e0720cd
AkashAnandAtkare <31027471+AkashAnandAtkare@users.noreply.github.com>
2017-08-22 01:36:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //move the image
 
 var image = document.getElementById('madi');
-madi.onClick= function(){
+madi.onclick= function(){
     madi.style.marginLeft = '100px';
      
 };
